ba7f41b7ec7e06ee4039980217c6cfeb40102d62
Quigonar <67380073+Quigonar@users.noreply.github.com>
2024-04-02 18:51:17 -0600

    a

---
+++ Lenguaje Alto Nivel/convolucion.asm
@@ -0,0 +1,21 @@
+LW r6, r6, #0       ;index for LOOP_AUDIO
+
+LOOP_AUDIO:
+    LW r7, r7, #0           ;index for SUMATORIA
+    LW r8, r8, #0           ;index of value to store
+    LDV b, r1               ;coeficientes b
+    LDV 255baudio, r2, r6   ;move len(b-1) 
+    MULV r3, r2, r1         ;mul b * x
+
+LOOP_SUMATORIA:
+    BEQ r6, #len(b)-1, FUERA_LOOP    ;mantener contador len(b) - 1
+    ROTV r4, r3, #1             ;rotar multiplicado
+    ADDV r3, r3, r4             ;sumar rotado con multiplicado
+    ADDI r7, r7, #1             ;sumar uno a indice de rotacion
+    JAL LOOP_SUMATORIA          ;loop
+
+FUERA_LOOP:
+    STV r3, r8              ;store vector r3 only value r8
+    ADDI r8, r8, #1         ;++ index of what to store
+    ADDI r6, r6, #1         ;++ audio part
+    JAL LOOP_AUDIO          ;go next audio part
