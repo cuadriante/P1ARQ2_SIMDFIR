856ffeb98a308c3badb21276eb753ba3d6b03fd4
Quigonar <67380073+Quigonar@users.noreply.github.com>
2024-04-09 17:26:22 -0600

    filtro y codigos

---
+++ Lenguaje Alto Nivel/compilador/inputEqual16.asm
@@ -0,0 +1,23 @@
+LW r6, r6, #0x16                            ;indice para muestra 16 elementos del audio real - 16 en memoria
+LW r8, r8, #0x48                            ;indice para guardar en memoria real - 16 en memoria
+LDV r1, #0x00                               ;coeficientes b CAMBIAR 0x00 por el espacio de memoria real
+
+LOOP_BETWEEN_AUDIOS:
+    ADDI r6, r6, #1                         ;+1 EN ESTE CASO B = 16 COEFICIENTES
+    LDV r2, r6                              ;muestra 16 elementos del audio
+
+LOOP_AUDIO:
+    LW r7, r7, #0                           ;index for SUMATORIA
+    MULV r3, r2, r1                         ;mul b * x
+
+LOOP_SUMATORIA:
+    BEQ r7, #16, FUERA_LOOP                 ;mantener contador len(b) - 1
+    ROTV r4, r3, #1                         ;rotar multiplicado
+    ADDV r4, r3, r4                         ;sumar rotado con multiplicado
+    ADDI r7, r7, #1                         ;sumar uno a loop de rotacion
+    JAL r5, LOOP_SUMATORIA                  ;loop hasta r7 = 16
+
+FUERA_LOOP:
+    STV r4, r8                              ;guardar el vector r4, en espacio de memoria r9
+    ADDI r8, r8, #1                         ;+1 al indice para guardar
+    JAL r5, LOOP_BETWEEN_AUDIOS             ;continuar con el siguiente indice r8, pero la misma parte del audio
